6ab430df0a600ea48a35934581bdd2569e4039f4
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-04-23 14:29:25 +0800

    Add files via upload

--- send_email.py
+++ send_email.py
@@ -18,27 +18,31 @@ def email_sender(mail_host,mail_user,mail_pass,sender,receivers,context,content,
     #接受方信息     
     msg['To'] = ','.join(receivers)  
     #html_img = f'<p>{content}<br><img src="cid:image1"></br></p>'
-    explain = '大周期指标（按年记）：Puell Multiple，BTC MVRV Z-Score，RHODL Ratio， 50MA aSOPR \
-               中周期指标（按周记）：7MA NRPL，7MA aSOPR \
-               短周期指标（按日记）：aSOPR，Futures Long Liquidations Dominance \
-               Puell Multiple：大于4预示牛顶，小于0.5预示熊底 \
-               BTC MVRV Z-Score: 大于7预示牛顶，小于0预示熊底 \
-               RHODL Ratio: 大于10.8预示牛顶，小于5.86预示熊底 \
-               50MA aSOPR :小于0.94预示熊底 \
-               7MA NRPL: 小于0，预示短期底部，可以进行低吸 \
-               7MA aSOPR：小于1，预示短期底部，可以进行低吸 \
-               aSOPR：小于1，预示全网陷入亏损，可以尝试定投 \
-               ETH P/BTC P: ETH价格/BTC价格，可以观察ETH的价格是不是被低估 \
+    message = MIMEText(content,'html','utf-8')
+    msg.attach(message)
+
+    explain = '大周期指标（按年记）：Puell Multiple，BTC MVRV Z-Score，RHODL Ratio， 50MA aSOPR \n \
+               中周期指标（按周记）：7MA NRPL，7MA aSOPR   \n \
+               短周期指标（按日记）：aSOPR，Futures Long Liquidations Dominance  \n \
+               Puell Multiple：大于4预示牛顶，小于0.5预示熊底  \n \
+               BTC MVRV Z-Score: 大于7预示牛顶，小于0预示熊底   \n \
+               RHODL Ratio: 大于10.8预示牛顶，小于5.86预示熊底   \n \
+               50MA aSOPR :小于0.94预示熊底  \n \
+               7MA NRPL: 小于0，预示短期底部，可以进行低吸   \n \
+               7MA aSOPR：小于1，预示短期底部，可以进行低吸   \n \
+               aSOPR：小于1，预示全网陷入亏损，可以尝试定投  \n \
+               ETH P/BTC P: ETH价格/BTC价格，可以观察ETH的价格是不是被低估  \n \
                Futures Long Liquidations Dominance: 大于0.5说明合约中多头优势，小于0.5说明合约中空头优势 '
     message_1 = MIMEText(explain,'html','utf-8')
+    message_1.add_header('Content-Disposition', 'attachment', filename='图表中的指标解释')
     msg.attach(message_1)
 
     text = '按照合约占比投资，准确率为：'+str(zhunquelv)
     message_2 = MIMEText(text,'html','utf-8')
+    message_2.add_header('Content-Disposition', 'attachment', filename='按照多空比投资准确率')
     msg.attach(message_2)
 
-    message = MIMEText(content,'html','utf-8')
-    msg.attach(message)
+
 
     jpgpart = MIMEImage(open('/root/meaasge_pre/chain_data_picture.png', 'rb').read())
     jpgpart.add_header('Content-Disposition', 'attachment', filename='chain_data_picture.jpg')
